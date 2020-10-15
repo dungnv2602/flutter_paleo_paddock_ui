6fc1de21fab44982b352ea97cc44ed0e0c7f2a10
dungnv2602 <dungnv2602@gmail.com>
2020-10-15 15:14:19 +0700

    fix clipping

--- lib/widgets/restaurant_card.dart
+++ lib/widgets/restaurant_card.dart
@@ -40,77 +40,81 @@ class RestaurantCard extends StatelessWidget {
             children: [
               const Spacer(),
               const Spacer(),
-              Expanded(
-                child: ClipPath(
-                  clipper: _RestaurantDetailsClipper(),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
+              ClipPath(
+                clipper: _RestaurantDetailsClipper(
+                  height: 20,
+                ),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8),
                     ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: model.iconColor,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: Icon(
-                              model.icon,
-                              color: Colors.white,
-                            ),
+                  ),
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: model.iconColor,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Icon(
+                            model.icon,
+                            color: Colors.white,
                           ),
                         ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                model.title,
-                                style: context.headline6,
-                              ),
-                              Text(
-                                model.subtitle,
-                                style: context.bodyText1
-                                    .letterSpace(1)
-                                    .textColor(_kGray),
-                              ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              model.title,
+                              style: context.headline6,
+                            ),
+                            Text(
+                              model.subtitle,
+                              style: context.bodyText1
+                                  .letterSpace(1)
+                                  .textColor(_kGray),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 2,
+                        height: 70,
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.white,
+                              Colors.white,
+                              _kGray,
                             ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
                           ),
                         ),
-                        Container(
-                          width: 2,
-                          height: 70,
-                          decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.white,
-                                Colors.white,
-                                _kGray,
-                              ],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          children: [
+                            const Icon(
+                              Icons.favorite_border,
+                              color: Colors.red,
                             ),
-                          ),
+                            Text('${model.likes}'),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            children: [
-                              const Icon(
-                                Icons.favorite_border,
-                                color: Colors.red,
-                              ),
-                              Text('${model.likes}'),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -144,7 +148,12 @@ class RestaurantCard extends StatelessWidget {
 }
 
 class _RestaurantDetailsClipper extends CustomClipper<Path> {
-  _RestaurantDetailsClipper() : path = Path();
+  _RestaurantDetailsClipper({
+    this.height = 20,
+    this.density = 50,
+  }) : path = Path();
+  final double height;
+  final double density;
 
   static const _radius = Radius.circular(5);
 
@@ -152,23 +161,26 @@ class _RestaurantDetailsClipper extends CustomClipper<Path> {
 
   @override
   Path getClip(Size size) {
-    const curYPos = 5.0;
-    double curXPos = 0.0;
+    path.lineTo(0.0, height);
 
-    final increment = size.width / 50;
+    double curXPos = 0.0;
+    final curYPos = height;
+
+    final increment = size.width / density;
 
     while (curXPos < size.width) {
       curXPos += increment;
       path.arcToPoint(Offset(curXPos, curYPos), radius: _radius);
     }
 
-    // close path
+    path.lineTo(size.width, 0.0);
     path.lineTo(size.width, size.height);
     path.lineTo(0.0, size.height);
     path.close();
+
     return path;
   }
 
   @override
-  bool shouldReclip(_RestaurantDetailsClipper oldClipper) => false;
+  bool shouldReclip(_RestaurantDetailsClipper oldClipper) => true;
 }
